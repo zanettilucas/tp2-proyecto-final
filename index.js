bb6abb2903bb32fffec892bd165eeafceeabbeaf
unknown <merlogm@yahoo.com>
2019-10-21 15:43:41 -0300

    prueba de concepto, distancia

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const controllers = require('./controllers');
+var distance = require('google-distance-matrix');
 
 const app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +19,37 @@ app.use((req, res, next) => {
 });
 */
 // --- Response standardization
+
+var origins = ['-34.609999, -58.429088'];
+var destinations = ['-34.603651, -58.381678'];
+
+
+distance.key('AIzaSyDSDU_29QYkLeBel6eA_7qygQ7A8M8bayk')
+ 
+distance.matrix(origins, destinations, function (err, distances) {
+  if (err) {
+      return console.log(err);
+  }
+  if(!distances) {
+      return console.log('no distances');
+  }
+  if (distances.status == 'OK') {
+      for (var i=0; i < origins.length; i++) {
+          for (var j = 0; j < destinations.length; j++) {
+              var origin = distances.origin_addresses[i];
+              var destination = distances.destination_addresses[j];
+              if (distances.rows[0].elements[j].status == 'OK') {
+                  var distance = distances.rows[i].elements[j].distance.text;
+                  var duration = distances.rows[i].elements[j].duration.text;
+                  console.log('La distancia desde: ' + origin + ' hasta: ' + destination + ' es ' + distance + ' / ' + duration);
+              } else {
+                  console.log(destination + ' is not reachable by land from ' + origin);
+              }
+          }
+      }
+  }
+});
+
 app.use((req, res, next) => {
   res.sendData = function sendData(data) {
     const response = {
