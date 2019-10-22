1edd51a76af0bedb6b25ee0150e20c970ffcfff0
zanettilucas <44378944+zanettilucas@users.noreply.github.com>
2019-10-22 20:35:53 -0300

    Se actualiza confirmar-turno

--- controllers/index.js
+++ controllers/index.js
@@ -4,16 +4,20 @@ const initializeRoutes = (app) => {
   app.use('/status', (req, res) => {
     res.sendData({ message: "I'm alive and well. Thank you." });
   });
-  app.use('/confirmar-turno', async (req, res) => {
-    await mailerService.sendConfirmOrder({
-      paciente: { email: 'paciente@mail.com' },
-      medico: { email: 'paciente@mail.com' },
-      codigo: 'turno sarlanga',
-      fecha: '10/02/1993',
-      horario: '10 pm',
-    });
-
-    res.sendData({ message: 'Mail enviado.' });
+  app.use('/confirmar-turno', async (req, res, next) => {
+    try {
+      await mailerService.sendConfirmOrder({
+        paciente: { email: 'paciente@mail.com' },
+        medico: { email: 'paciente@mail.com' },
+        codigo: 'turno sarlanga',
+        fecha: '10/02/1993',
+        horario: '11 am',
+      });
+      res.sendData({ message: 'Mail enviado.' });
+    } catch (e) {
+      res.status(418).send({ error: 'Las teteras no envían mails.' });
+      next(e);
+    }
   });
 };
 module.exports = { initializeRoutes };
