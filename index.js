700da7ae252924ad79ee2e121ee4a36b89f6ad7b
Lemos, Denisse A <denisse.a.lemos@accenture.com>
2019-10-29 21:14:27 -0300

    Pusheo porque anda

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-// import models from './models';
 const bodyParser = require('body-parser');
 const express = require('express');
+const env = require('dotenv').load();
+const models = require('./models');
 const controllers = require('./controllers');
+
 
 const app = express();
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-/* descomentar cuando models tenga algo.
+
 app.use((req, res, next) => {
   // save the models in the context
   req.context = {
@@ -15,7 +18,7 @@ app.use((req, res, next) => {
   };
   next();
 });
-*/
+
 // --- Response standardization
 app.use((req, res, next) => {
   res.sendData = function sendData(data) {
@@ -39,8 +42,21 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// app.listen(config.port, () => {
 app.listen(3000, () => {
-//    console.log(`Express server listening on port ${config.port}`);
+
   console.log('Express server listening on port 3000');
 });
+
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+  console.log('Nice! Database looks fine')
+
+}).catch(function(err) {
+
+  console.log(err, "Something went wrong with the Database Update!")
+
+}
+
+
+);
