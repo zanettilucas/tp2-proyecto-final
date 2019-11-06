a1f13ff27867f12e84ec974bd93b9430205cf68e
unknown <merlogm@yahoo.com>
2019-11-05 21:20:31 -0300

    modularizando

--- controllers/index.js
+++ controllers/index.js
@@ -1,35 +1,6 @@
 const fs = require('fs');
 const mailerService = require('../services/mailer.service');
-const distanceService = require('../services/distance.service');
-
-const medicos = [
-  {
-    nombre: 'carlos',
-    apellido: 'gomez',
-    id: '1',
-    ubicacion: '-34.603651, -58.381678',
-  },
-  {
-    nombre: 'jose',
-    apellido: 'perez',
-    id: '2',
-    ubicacion: '-34.603651, -58.381678',
-  },
-];
-const pacientes = [
-  {
-    nombre: 'lucas',
-    apellido: 'jimenez',
-    id: '3',
-    ubicacion: '-34.609999, -58.429088',
-  },
-  {
-    nombre: 'pablo',
-    apellido: 'perez',
-    id: '4',
-    ubicacion: '-34.609999, -58.429088',
-  },
-];
+const utilsService = require('../services/utils.service');
 
 const initializeRoutes = (app) => {
   app.use('/status', (req, res) => {
@@ -51,40 +22,13 @@ const initializeRoutes = (app) => {
     }
   });
 */
-  function obtenerUbicacionMedico(idMedico) {
-    // fs.readFile('./data/medicos.json', (err, data) => {
-    //   if (err) throw err;
-    //   const medicos = JSON.parse(data);
-    //   console.log(medicos.ubicacion);
-    // });
-    const { ubicacion } = medicos.find((x) => x.id === idMedico);
-    return ubicacion;
-  }
-
-  function obtenerUbicacionPaciente(idPaciente) {
-    // fs.readFile('./data/medicos.json', (err, data) => {
-    //   if (err) throw err;
-    //   const medicos = JSON.parse(data);
-    //   console.log(medicos.ubicacion);
-    // });
-    const { ubicacion } = pacientes.find((x) => x.id === idPaciente);
-    return ubicacion;
-  }
-
-  const calcularDistanciaMP = async (pacienteId, medicoId) => {
-    const ubicacionPaciente = obtenerUbicacionPaciente(pacienteId);
-    const ubicacionMedico = obtenerUbicacionMedico(medicoId);
-    const distanciaYDuracion = await distanceService.calcularDistancia(ubicacionPaciente, ubicacionMedico);
-    return distanciaYDuracion;
-  };
-
 
   app.use('/medico/:medicoId/paciente/:pacienteId', async (req, res, next) => {
     try {
       if (req.method === 'GET') {
         // const disYD = await distanceService.calcularDistancia
         // (req.query.paciente, req.query.medico);
-        const distanciaYDuracion = await calcularDistanciaMP(req.params.pacienteId, req.params.medicoId);
+        const distanciaYDuracion = await utilsService.calcularDistanciaMP(req.params.pacienteId, req.params.medicoId);
         console.log('distanciaYDuracion');
         console.log(distanciaYDuracion);
         res.sendData({ message: distanciaYDuracion });
